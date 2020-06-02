5690faaf04557e3604b22f4da6d9eb6c4db055d4
arielveloso <el_veloso@hotmail.com>
2020-06-02 14:00:34 +0200

    ejercicio

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
+
 const restPhoneApi = axios.create({
-    baseURL: 'https://glacial-reef-88309.herokuapp.com/api/persons'
+    //baseURL: 'https://glacial-reef-88309.herokuapp.com/api/persons'
+    baseURL: 'http://localhost:9000'
 })
 const getAllPhone = () => {
     restPhoneApi
-        .get()
+        .get('/api/persons')
         .then(responseFromApi => {
             console.log(responseFromApi.data);
             document.write(JSON.stringify(responseFromApi.data))
         })
         .catch(err => console.log('error', err));
 }
+
+const hacerPost = () => {
+    const credentials = {
+        user: document.getElementById('user').value,
+        password: document.getElementById('password').value
+    }
+
+    restPhoneApi
+        .post('/login', credentials)
+        .then(console.log('login successful'))
+        .catch(err => console.log('error', err));
+}
+
+const hacerLogout = () => {
+    restPhoneApi
+        .get('/logot')
+        .then(responseFromApi => {
+            console.log(responseFromApi.data);
+        })
+        .catch(err => console.log('error', err));
+}
+
 document.getElementById('theButton').onclick = getAllPhone;
+
+document.getElementById('login').onclick = hacerPost;
+document.getElementById('login').onclick = hacerLogout;
+
